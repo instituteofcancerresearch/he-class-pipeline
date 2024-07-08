11f97977001352e4c53b9b0107774d86bd02e2b6
ralcraft <rachel.alcraft@icr.ac.uk>
2024-07-08 14:23:21 +0100

    simpliofy sbatch

--- codeb/sbatch.sh
+++ codeb/sbatch.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 
 #SBATCH -J "HEBr"
-#SBATCH -o /data/scratch/DCO/DIGOPS/SCIENCOM/ralcraft/he-classifier/he-class-pipeline/outB/srun_b.out
-#SBATCH -e /data/scratch/DCO/DIGOPS/SCIENCOM/ralcraft/he-classifier/he-class-pipeline/outB/srun_b.err
+#SBATCH -o ./outB/srun_b.out
+#SBATCH -e ./outB/srun_b.err
 #SBATCH -n 4
 #SBATCH --mail-type="END,FAIL"
 #SBATCH -t 100:00:00
 
 module load MATLAB/R2020b
-
-#ImageTilesPath="/data/scratch/DMP/UCEC/GENEVOD/ntrahearn/Tiles/AdInCar/ICR/Tiles/"
-ImageTilesPath="/data/scratch/DCO/DIGOPS/SCIENCOM/ralcraft/he-classifier/he-class-pipeline/outA/"
-#MaskTilesPath="/data/scratch/DMP/UCEC/GENEVOD/ntrahearn/Tiles/AdInCar/ICR/Masks/"
-MaskTilesPath="/data/scratch/DCO/DIGOPS/SCIENCOM/ralcraft/he-classifier/he-class-pipeline/outB/"
-
-MaskMethod="E";
-Params="'jpg', [3.5 500 225]"
-
-#codePath="/data/scratch/DMP/UCEC/GENEVOD/ntrahearn/Code/MaskTiles/"
-codePath="/data/scratch/DCO/DIGOPS/SCIENCOM/ralcraft/he-classifier/he-class-pipeline/codeb/"
-
-
-
-module load MATLAB
-
-#matlab -nodesktop -nosplash -r "addpath(genpath('${codePath}')); CreateMaskTilesBatch('$ImageTilesPath', '$MaskTilesPath', '$MaskMethod', $Params); exit;"
 matlab -nodesktop -nosplash -r "addpath(genpath('./he-class-pipeline/codeb/')); CreateMaskTilesBatch('./outA/', './outB/', '"E"'); exit;"
 
