6f02ed66ce07f6002a2bcf49fc8133216929f69d
ralcraft <rachel.alcraft@icr.ac.uk>
2024-09-11 22:11:14 +0100

    added files

--- pipeline-b/SBatch_maker.py
+++ pipeline-b/SBatch_maker.py
@@ -11,7 +11,7 @@ outB = sys.argv[4]
 batch_file = f"{working_dir}/b_run.sh"
 
 with open(batch_file, "w") as f:
-    f.write("#!/bin/sh")
+    f.write("#!/bin/sh\n")
 
 with open(batch_file, "a") as f:
     f.write("#SBATCH -J HEBr\n")
@@ -22,12 +22,12 @@ with open(batch_file, "a") as f:
     f.write("source ~/.bashrc\n")
     f.write("module load MATLAB/R2020b\n")
     f.write("echo 'Starting HEBr'\n")
-    #f.write("matlab -nodesktop -nosplash -r ")
-    #f.write('"')
-    #f.write("addpath(genpath('./he-class-pipeline/pipeline-b/')); ")
-    #f.write("CreateMaskTilesBatch('./outA/', './outB/', '")
-    #f.write('"T"')    
-    #f.write("'); exit;")
+    f.write("matlab -nodesktop -nosplash -r ")
+    f.write('"')
+    f.write(f"addpath(genpath('{pipeline_dir}/pipeline-b/')); ")
+    f.write(f"CreateMaskTilesBatch('{inB}', '{outB}', '")
+    f.write('"T"')    
+    f.write("'); exit;")
 """
 #!/bin/sh
 #SBATCH -J "HEBr"
