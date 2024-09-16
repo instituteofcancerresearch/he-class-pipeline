d7ae2cad5cad28250f9b5f6915c01415e105a41d
ralcraft <rachel.alcraft@icr.ac.uk>
2024-09-16 18:10:58 +0100

    rem number from jobname

--- pipeline-d/rse_wrap.sh
+++ pipeline-d/rse_wrap.sh
@@ -19,7 +19,7 @@ do
   echo "Steps=$steps"  
   var1="logsD/d_run_$counter.err"
   var2="logsD/d_run_$counter.out"  
-  jabname="HEDr$counter"  
+  jabname="HEDr"  
   sbatch --error=$var1 --output=$var2 --job-name=$jobname "$pipe_path/rse_sbatch_single.sh" $image_file $code_path $steps
   ((counter++))
 done
