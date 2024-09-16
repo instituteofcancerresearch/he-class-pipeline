096577360c072daad19ad3f85730999d4802e033
ralcraft <rachel.alcraft@icr.ac.uk>
2024-09-16 08:28:08 +0100

    moved logs after sbatch

--- pipeline-c/SBatch_maker.py
+++ pipeline-c/SBatch_maker.py
@@ -37,17 +37,16 @@ with open(batch_file, "a") as f:
     sing_call += "-B {inC_refMasks}:/target_masks "
     sing_call += "{sing_dir}/HEC.sif "
     sing_call += "/log /input_tiles /target_tiles /output /input_masks /target_masks *.*"
-    
-    f.write(f"echo '********'\n")    
-    f.write(f"echo '{sing_call}'\n")    
-    f.write(f"echo '********'\n")       
-    
+            
     f.write("#SBATCH -J HECr\n")
     f.write("#SBATCH -o c_run.out\n")
     f.write("#SBATCH -e c_run.err\n")
     f.write("#SBATCH -n 4\n")
     f.write("#SBATCH -t 100:00:00\n")
     f.write("source ~/.bashrc\n")        
+    f.write(f"echo '********'\n")    
+    f.write(f"echo '{sing_call}'\n")    
+    f.write(f"echo '********'\n")       
     f.write("echo 'Starting HECr'\n")        
     f.write("srun singularity run ")    
     f.write(f"-B {inC_tiles}:/input_tiles ")    
