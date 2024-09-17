a62ca0e5ba96a5f23f142179cd94f3e8f598ca41
ralcraft <rachel.alcraft@icr.ac.uk>
2024-09-17 15:47:26 +0100

    dot

--- pipeline-d/code/cell_detector/analysis/sccnn_detection.py
+++ pipeline-d/code/cell_detector/analysis/sccnn_detection.py
@@ -4,13 +4,14 @@ import stain_normalization as sn # not needed forces path recognition
 import os,sys
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
+sys.path.append(str(dir_path) + "/subpackages")
 
-from subpackages import variable_summaries
-from subpackages import train_op
-from subpackages import loss_function
-from subpackages import inference
-from subpackages import generate_output
-from subpackages import run_training
+from .subpackages import variable_summaries
+from .subpackages import train_op
+from .subpackages import loss_function
+from .subpackages import inference
+from .subpackages import generate_output
+from .subpackages import run_training
 
 
 class SCCNN:
