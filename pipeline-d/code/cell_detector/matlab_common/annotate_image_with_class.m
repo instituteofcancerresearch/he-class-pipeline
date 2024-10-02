88b3edc3985e4fb733300ddc1ca69af29cf93204
ralcraft <rachel.alcraft@icr.ac.uk>
2024-10-02 12:56:54 +0100

    more logging

--- pipeline-d/code/cell_detector/matlab_common/annotate_image_with_class.m
+++ pipeline-d/code/cell_detector/matlab_common/annotate_image_with_class.m
@@ -18,30 +18,33 @@ function image = annotate_image_with_class(image, points, colour, strength)
     disp("...7")
     label = imdilate(label, strel('disk', strength))>0;
     %     label = logical(repmat(label, [1, 1, 3]));
-    disp("...8")    
+    disp("...8");   
     image1 = image(:,:,1);
-    disp("...9")
+    disp("...9");
     image2 = image(:,:,2);
-    disp("...10")
+    disp("...10");
     image3 = image(:,:,3);
-    disp("...11")
+    disp("...11");
     image1 = image1(:);
-    disp("...12")
+    disp("...12");
     image2 = image2(:);
-    disp("...13")
+    disp("...13");
     image3 = image3(:);
-    disp("...14")
+    disp("...14");
+    disp(colour);
+    disp(colour(1));
     image1(label(:)) = colour(1);
-    disp("...15")
+    disp("...15");
+    disp(colour(2));
     image2(label(:)) = colour(2);
-    disp("...16")
+    disp("...16");
     image3(label(:)) = colour(3);
-    disp("...17")
+    disp("...17");
     image1 = reshape(image1, size(label));
-    disp("...18")
+    disp("...18");
     image2 = reshape(image2, size(label));
-    disp("...19")
+    disp("...19");
     image3 = reshape(image3, size(label));
-    disp("...20")
+    disp("...20");
     image = cat(3, image1, image2, image3);
 end
