872f4d4612c8b6f7c68bd2be434606ce5f017def
Anastasiia Shcherbakova <ashcherbakova@35481intlt.icr.ac.uk>
2024-11-11 17:49:29 +0000

    made script more flexible for other purposes

--- utils/compare-images.py
+++ utils/compare-images.py
@@ -3,9 +3,9 @@ from PIL import Image
 import numpy as np
 import cv2
 # image_similarity_measures taken from: https://github.com/nekhtiari/image-similarity-measures
-import image_similarity_measures
 from image_similarity_measures.quality_metrics import rmse, sre
 import os
+import glob
 
 def compare_images(path_1, path_2):
     """
@@ -17,31 +17,32 @@ def compare_images(path_1, path_2):
         RMSE - measures the amount of change per pixel due to the processing.
 
     """
-    ## TODO: READ THE FOLDERS WHICH ARE CONTAINED WITHIN EACH DIRECTORY AND THE FOLDERS LIST
-    step_lit_reg = ['outA-Tiles', 'outB-Masks', 'outC-Norm']
-    step_lit_test = ['outA', 'outB', 'outC']
-    folder_list = ['SS-05-14545-1A', 'SS-05-21490-1A', 'SS-08-12230-3A', 'SS-08-12436-1A', 'SS-09-24500-2A']
-    # valid_extensions = ['.jpg', '.png']
 
-    for step in range(len(step_lit_reg)):
-        count = 0
-        print(f"Analysing step {step_lit_test[step]}")
-        folder_path1 = path_1 + step_lit_reg[step]
-        folder_path2 = path_2 + step_lit_test[step]
+    folder_list1 = sorted(glob.glob(path_1+"/out*"))
+    folder_list2 = sorted(glob.glob(path_2+"/out*"))
+    # print(folder_list1)
+    # print(folder_list2)
+    for folder1, folder2 in zip(folder_list1, folder_list2):
+        subfolder_list1 = sorted(glob.glob(folder1+"/*.ndpi"))
+        subfolder_list2 = sorted(glob.glob(folder2+"/*.ndpi"))
+        # print(subfolder_list1)
+        # print(subfolder_list2)
+        for subfolder1, subfolder2 in zip(subfolder_list1, subfolder_list2):
+            print(f"In folder {subfolder1}")
+            file_num1 = len(glob.glob(subfolder1+"/Da*"))
+            file_num2 = len(glob.glob(subfolder2+"/Da*"))
 
-        for folder in folder_list:
-            folder_name = (f'{folder}.ndpi')
-            print(f"In folder {step_lit_test[step]}/{folder_name}")
-
-            ## TODO: COUNT THE NUMBER OF FILES IN EACH FOLDER
-            for i in range(0, 35):
-                image1_base = f'{folder_path1+folder_name}/Da{i}'
-                image2_base = f'{folder_path2+folder_name}/Da{i}'
+            for i in range(0, (file_num1 + 1)):
+                count = 0
+                image1_base = f'{subfolder1}/Da{i}'
+                image2_base = f'{subfolder2}/Da{i}'
 
                 for ext in ['.jpg', '.png']:
                     try:
                         image1_path = (f'{image1_base}{ext}')
                         image2_path = (f'{image2_base}{ext}')
+                        # print(image1_path)
+                        # print(image2_path)
                         
                         image1 = Image.open(image1_path)
                         image2 = Image.open(image2_path)
@@ -58,6 +59,9 @@ def compare_images(path_1, path_2):
                             # out_sre = sre(image1_cv2, image2_cv2)
                             print(f"RMSE image difference: {out_rmse}")
                             # print(f"SRE image difference: {out_sre}")
+                        
+                        # else:
+                        #     print(f"Da{i}: The images are the same.")
 
                     except FileNotFoundError:
                         continue
@@ -67,9 +71,11 @@ def compare_images(path_1, path_2):
                 # else:
                 #     print(f"Da{i}: No image file found with .jpg or .png extensions.")
 
-            print(f"{count} different images in folder {folder}")
+            # print(f"{count} different images in folder {folder}")
 
-path1 = "/Users/ashcherbakova/Projects/he-class-pipeline/regression/"
-path2 = "/Users/ashcherbakova/Desktop/HE_test/"
+# Change paths to the folders with images you want to compare, folders have to have the same 
+# names for files that are being compared
+path1 = "/Users/ashcherbakova/Desktop/HE_test"
+path2 = "/Users/ashcherbakova/Projects/he-class-pipeline/regression"
 
 compare_images(path1, path2)
