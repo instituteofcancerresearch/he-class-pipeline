68b0fbdc6e96971b0ebba41eb6a2254c9f4c4f69
Anastasiia Shcherbakova <ashcherbakova@35481intlt.icr.ac.uk>
2024-11-20 13:50:28 +0000

    updated bash script

--- utils/compare-images.sh
+++ utils/compare-images.sh
@@ -6,11 +6,13 @@ cd /data/scratch/DCO/DIGOPS/SCIENCOM/ashcherbakova/he-app-test/
 
 # conda init
 echo -e "Activating conda enviroment..."
-conda activate comprate-images-env
+# conda activate comprate-images-env
+mamba activate comprate-images-env
 
 echo -e "Running compare images script..."
 python3 compare-images.py "$1" "$2" > compare-images-result.log 2> compare-images-result.err
 
 # conda init
 echo -e "Deactivating conda enviroment..."
-conda deactivate
+# conda deactivate
+mamba deactivate
