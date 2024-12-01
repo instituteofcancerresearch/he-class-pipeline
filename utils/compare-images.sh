f9a6f85de1e922912659e56d2ce179cb769a6162
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2024-12-01 18:01:50 +0000

    Allow a list of image stages to be passed in

--- utils/compare-images.sh
+++ utils/compare-images.sh
@@ -9,6 +9,7 @@ image_dir_b=$4
 image_dir_c=$5
 image_dir_d1=$6
 image_dir_d2=$7
+pipes=$8 
 
 # Fixed comparison directories
 image_comp_a="${pipeline_path}/regression/outA-Tiles"
@@ -21,6 +22,7 @@ python_script="${pipeline_path}/utils/compare-images.py"
 echo "*********INPUTS***********************"
 echo "pipe_path: $pipeline_path"
 echo "conda_path: $conda_path"
+echo "pipes: $pipes"
 echo "python_script: $python_script"
 echo "image_dir_a: $image_dir_a"
 echo "image_dir_b: $image_dir_b"
@@ -38,20 +40,30 @@ echo -e "Activating conda enviroment..."
 # conda activate comprate-images-env
 mamba activate $conda_path
 
-echo -e "Running compare images script for A..."
-srun python $python_script "$image_dir_a" "$image_comp_a" "N"
+if [[ $steps == *"A"* ]]; then
+    echo -e "Running compare images script for A..."
+    srun python $python_script "$image_dir_a" "$image_comp_a" "N"
+fi
 
-echo -e "Running compare images script for B..."
-srun python $python_script "$image_dir_b" "$image_comp_b" "N"
+if [[ $steps == *"B"* ]]; then
+    echo -e "Running compare images script for B..."
+    srun python $python_script "$image_dir_b" "$image_comp_b" "N"
+fi
 
-echo -e "Running compare images script for C..."
-srun python $python_script "$image_dir_c" "$image_comp_c" "N"
+if [[ $steps == *"C"* ]]; then
+    echo -e "Running compare images script for C..."
+    srun python $python_script "$image_dir_c" "$image_comp_c" "N"
+fi
 
-echo -e "Running compare images script for D1..."
-srun python $python_script "$image_dir_d1" "$image_comp_d1" "N"
+if [[ $steps == *"D1"* ]]; then
+    echo -e "Running compare images script for D1..."
+    srun python $python_script "$image_dir_d1" "$image_comp_d1" "N"
+fi
 
-echo -e "Running compare images script for D2..."
-srun python $python_script "$image_dir_d2" "$image_comp_d2" "N"
+if [[ $steps == *"D2"* ]]; then
+    echo -e "Running compare images script for D2..."
+    srun python $python_script "$image_dir_d2" "$image_comp_d2" "N"
+fi
 
 echo -e "Deactivating conda enviroment..."
 mamba deactivate
