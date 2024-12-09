eeb314b8578fa1d1725956cf3a5ffc302ac1f134
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2024-12-09 18:05:35 +0000

    specify d1 h5 params

--- utils/compare_h5_csv.py
+++ utils/compare_h5_csv.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import pandas as pd
 
-def compare_h5(data_path_1, data_path_2):
+def compare_h5(data_path_1, data_path_2, key):
 
     are_the_same = True
     h5_file_1 = h5py.File(data_path_1, 'r')
@@ -15,9 +15,14 @@ def compare_h5(data_path_1, data_path_2):
 
     if groups_1 == groups_2:
         for group_1 in groups_1:
-            datasets_1 = list(h5_file_1[group_1].items())
-            datasets_2 = list(h5_file_2[group_1].items())
-
+            if key.upper() == "ALL":
+                datasets_1 = list(h5_file_1[group_1].items())
+                datasets_2 = list(h5_file_2[group_1].items())
+            else:
+                datasets_1 = [key]
+                datasets_2 = [key]
+                
+            
         if datasets_1 == datasets_2:
             for (dataset_1_name, dataset_1), (dataset_2_name, dataset_2) in zip(datasets_1, datasets_2):
                 if dataset_1_name == dataset_2_name and dataset_1.shape == dataset_2.shape:
