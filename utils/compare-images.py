67b7de94de0b4c4866dcaa277760bc7837216b8c
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2024-12-09 21:47:32 +0000

    SS or Da for matches

--- utils/compare-images.py
+++ utils/compare-images.py
@@ -68,8 +68,12 @@ def compare_images(path_new, path_regression, recursive, ndpi, key):
                 subfolder1 = folder1
                 subfolder2 = folder2
             print(f"Compare folders {subfolder1} {subfolder2}")
-            files_1tmp = sorted(glob.glob(subfolder1+"/*"))
-            files_2tmp = sorted(glob.glob(subfolder2+"/*"))            
+            if ndpi == "Y":
+                files_1tmp = sorted(glob.glob(subfolder1+"/Da*"))
+                files_2tmp = sorted(glob.glob(subfolder2+"/Da*"))            
+            else:
+                files_1tmp = sorted(glob.glob(subfolder1+"/SS*"))
+                files_2tmp = sorted(glob.glob(subfolder2+"/SS*"))
             files_1 = []
             files_2 = []            
             for file in files_1tmp:
